67d4437956e9d2e0c38f9b1f4d98406af7567dd1
huhn511 <sehe89@gmail.com>
2020-06-22 13:59:14 +0200

    :sparkles: add website donations

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const { Client, MessageEmbed } = require('discord.js');
 const client = new Client();
+const request = require('request');
 
 const config = require('dotenv').config().parsed // Loads .env
 
 // functions
 var cmds = {
-    help
+    help,
+    tip
 }
 
-const PREFIX = "$"
+const PREFIX = "!"
+const COLOR = "#0FC1B7"
 
 client.on('message', async msg => {
     try {
         //return if other channel
-        if (msg.channel.id != config.CHANNEL_ID) return
+        // if (msg.channel.id != config.CHANNEL_ID) return
         // Getting content, author and channel out of message object
         let cont = msg.content,
             author = msg.author
@@ -43,10 +46,56 @@ client.on('ready', () => {
 function help(msg) {
     const embed = new MessageEmbed()
         // Set the title of the field
-        .setTitle('A slick little embed')
+        .setTitle('Help - IOTA Tipbot')
         // Set the color of the embed
-        .setColor(0xff0000)
+        .setColor(COLOR)
         // Set the main content of the embed
-        .setDescription('Hello, this is a slick embed!');
+        .setDescription('How can i help you?');
     msg.channel.send(embed);
+}
+
+function tip(msg, args) {
+
+    console.log("tip to: ", args)
+
+    if (validURL(args[0])) {
+        request('https://raw.githubusercontent.com/trusty-code/tipbot-backend/master/data/websites.json', { json: true }, (err, res, body) => {
+            if (err) { return console.log(err); }
+            var found = false;
+            for(var i = 0; i < body.length; i++) {
+                console.log(body[i].url);
+                console.log(args[0]);
+                if (body[i].url.toLowerCase().includes(args[0].toLowerCase())) {
+                    found = body[i];
+                    break;
+                }
+            }
+            if(found) {
+                const embed = new MessageEmbed()
+                    // Set the title of the field
+                    .setTitle(`Donate to: ${args[0]}`)
+                    // Set the color of the embed
+                    .setColor(COLOR)
+                    // Set the main content of the embed
+                    .setDescription(found.donation_address);
+                msg.channel.send(embed);
+            } else {
+                console.log("website not found.")
+                msg.channel.send("Website not found in Trustify registry :-(");
+            }
+            
+        });
+    } else {
+        msg.channel.send("tip to: ", args[0]);
+    }
+}
+
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return !!pattern.test(str);
 }
