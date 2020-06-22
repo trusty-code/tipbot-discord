6a7829902b00c6b1a7583d9c2bdfbcd41d00fbac
huhn511 <sehe89@gmail.com>
2020-06-22 21:07:54 +0200

    :checkered_flag: add qr code image

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const { Client, MessageEmbed } = require('discord.js');
+const { Client, MessageEmbed, MessageAttachment } = require('discord.js');
 const client = new Client();
 const request = require('request');
+const fs = require('fs');
+const express = require('express');
+const app = express();
 
 const config = require('dotenv').config().parsed // Loads .env
+
+var QRCode = require('qrcode')
+
+app.use('/qr_codes', express.static(__dirname + '/qr_codes'));
 
 // functions
 var cmds = {
@@ -55,14 +62,11 @@ function help(msg) {
 }
 
 function tip(msg, args) {
-
-    console.log("tip to: ", args)
-
     if (validURL(args[0])) {
         request('https://raw.githubusercontent.com/trusty-code/tipbot-backend/master/data/websites.json', { json: true }, (err, res, body) => {
             if (err) { return console.log(err); }
             var found = false;
-            for(var i = 0; i < body.length; i++) {
+            for (var i = 0; i < body.length; i++) {
                 console.log(body[i].url);
                 console.log(args[0]);
                 if (body[i].url.toLowerCase().includes(args[0].toLowerCase())) {
@@ -70,20 +74,31 @@ function tip(msg, args) {
                     break;
                 }
             }
-            if(found) {
-                const embed = new MessageEmbed()
-                    // Set the title of the field
-                    .setTitle(`Donate to: ${args[0]}`)
-                    // Set the color of the embed
-                    .setColor(COLOR)
-                    // Set the main content of the embed
-                    .setDescription(found.donation_address);
-                msg.channel.send(embed);
+            if (found) {
+                QRCode.toDataURL(found.donation_address).then((res) => {
+                    var base64Data = res.replace(/^data:image\/png;base64,/, "");
+
+                    fs.writeFileSync("./qr_codes/" + found.donation_address + ".jpg", base64Data, 'base64');
+                    // Now save the data
+                    const img_url = "http://localhost:3000/qr_codes/" + found.donation_address + ".jpg"
+                    console.log("img_url", img_url)
+                    const embed = new MessageEmbed()
+                        // Set the title of the field
+                        .setTitle(`Donate to: ${args[0]}`)
+                        // Set the color of the embed
+                        .setColor(COLOR)
+                        .attachFiles(img_url)
+                        // Set the main content of the embed
+                        .setDescription(img_url);
+
+                    msg.channel.send(embed);
+                }).catch(err => {
+                    console.error(err)
+                })
             } else {
                 console.log("website not found.")
                 msg.channel.send("Website not found in Trustify registry :-(");
             }
-            
         });
     } else {
         msg.channel.send("tip to: ", args[0]);
@@ -99,3 +114,7 @@ function validURL(str) {
         '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
     return !!pattern.test(str);
 }
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
