bb8e048130ce405ebd2fbf966b2bb7dbf968ee0e
huhn511 <sehe89@gmail.com>
2020-06-26 13:37:33 +0200

    :heavy_plus_sign: add address validation

--- src/commands/add.js
+++ src/commands/add.js
@@ -2,19 +2,22 @@ const { MessageEmbed } = require('discord.js');
 
 const { COLOR } = require('../config');
 const { addAddress } = require('../database');
+const checksum = require('@iota/checksum')
 
 module.exports =
-    function add(msg, args) {
+    async function add(msg, args) {
 
         let address = args[0];
 
-        if(isValidIotaAddress(address)) {
+        let isValid = await isValidIotaAddress(address)
+        console.log("isValid", isValid)
+        if (isValid) {
 
             addAddress(msg.author.id, address)
 
             const embed = new MessageEmbed()
-            // Set the title of the field
-            .setTitle('Add - IOTA Tipbot')
+                // Set the title of the field
+                .setTitle('Add - IOTA Tipbot')
                 // Set the color of the embed
                 .setColor(COLOR)
                 // Set the main content of the embed
@@ -24,13 +27,19 @@ module.exports =
             msg.channel.send("No Valid IOTA Address");
         }
 
-      
+
     }
 
 
-function isValidIotaAddress(address) {
+async function isValidIotaAddress(address) {
 
-    // TODO 
-    if(address) return true
-    return false
+    // Check if address has valid formats (90 chars: 81 + 9 for the checksum and just trytes (A-Z and 9)) 
+    try {
+        // Check if address has valid checksum
+        let isValid = checksum.isValidChecksum(address)
+        if(isValid) return true
+        return false 
+    } catch (error) {
+        return false
+    }
 }
