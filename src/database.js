e8fffdae48bc249b56973199eea0e648f85b6de7
huhn511 <sehe89@gmail.com>
2020-06-26 13:36:39 +0200

    :art: clean up db

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const Keyv = require('keyv')
 const KeyvFile = require('keyv-file').KeyvFile
-const os = require('os')
+const path = require('path');
+var appDir = path.dirname(require.main.filename);
 
 const keyv = new Keyv({
   store: new KeyvFile()
@@ -8,7 +9,7 @@ const keyv = new Keyv({
 // More options with default value:
 const customKeyv = new Keyv({
   store: new KeyvFile({
-    filename: `./default-rnd-${Math.random().toString(36).slice(2)}.json`, // the file path to store the data
+    filename: appDir + `${Math.random().toString(36).slice(2)}.json`, // the file path to store the data
     expiredCheckDelay: 24 * 3600 * 1000, // ms, check and remove expired data in each ms
     writeDelay: 100, // ms, batch write to disk in a specific duration, enhance write performance.
     encode: JSON.stringify, // serialize function
@@ -18,19 +19,11 @@ const customKeyv = new Keyv({
 
 
 async function addAddress(user_id, address) {
-
-    await keyv.set(user_id, address); // true
-
-    console.log("user_id: ", user_id)
-    console.log("address: ", address)
-
-
+    await keyv.set(user_id, address)
 }
 
 async function getAddress(user_id) {
-
-    console.log("user_id", user_id)
-    let address = await keyv.get(user_id); // true
+    let address = await keyv.get(user_id)
     return address;
 }
 
