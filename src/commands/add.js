568dbb09538d230287dce4f71bfd82c8f4a3a37d
huhn511 <sehe89@gmail.com>
2020-06-30 00:40:35 +0200

    :sparkles: add trustify bot nd add method

--- src/commands/add.js
+++ src/commands/add.js
@@ -2,44 +2,23 @@ const { MessageEmbed } = require('discord.js');
 
 const { COLOR } = require('../config');
 const { addAddress } = require('../database');
+const tipbot = require('@trustify/tipbot.ts');
 const checksum = require('@iota/checksum')
 
 module.exports =
     async function add(msg, args) {
 
         let address = args[0];
+        let user = String(msg.author.id);
+        let response = await tipbot.add(user, address)
 
-        let isValid = await isValidIotaAddress(address)
-        console.log("isValid", isValid)
-        if (isValid) {
+        const embed = new MessageEmbed()
+            // Set the title of the field
+            .setTitle('Add - IOTA Tipbot')
+            // Set the color of the embed
+            .setColor(COLOR)
+            // Set the main content of the embed
+            .setDescription(response);
 
-            addAddress(msg.author.id, address)
-
-            const embed = new MessageEmbed()
-                // Set the title of the field
-                .setTitle('Add - IOTA Tipbot')
-                // Set the color of the embed
-                .setColor(COLOR)
-                // Set the main content of the embed
-                .setDescription('Address added.');
-            msg.channel.send(embed);
-        } else {
-            msg.channel.send("No Valid IOTA Address");
-        }
-
-
+        msg.channel.send(embed);
     }
-
-
-async function isValidIotaAddress(address) {
-
-    // Check if address has valid formats (90 chars: 81 + 9 for the checksum and just trytes (A-Z and 9)) 
-    try {
-        // Check if address has valid checksum
-        let isValid = checksum.isValidChecksum(address)
-        if(isValid) return true
-        return false 
-    } catch (error) {
-        return false
-    }
-}
